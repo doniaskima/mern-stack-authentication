ed3d7bfa8db99aac85e77d34d6dc3f1fb2fede99
doniaskima <doniaskima344@gmail.com>
2022-08-11 00:24:44 +0100

    struggle

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,30 +1,48 @@
-import { createSlice, createAsynThunk } from '@reduxjs/toolkit';
-
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import authService from "./authService";
 
 // get user from localStorage
 
-const user = JSON.parse(localStorage.getItem('user'))
-    //because localStorage only have string 
+const user = JSON.parse(localStorage.getItem("user"));
+//because localStorage only have string
 const intialState = {
     user: user ? user : null,
     isError: false,
     isSuccess: false,
     isLoading: false,
-    message: ''
-}
+    message: "",
+};
+
+// Register user
+export const register = createAsyncThunk(
+    "auth/register",
+    async(user, thunkAPI) => {
+        try {
+            return await authService.register(user);
+        } catch (error) {
+            const message =
+                (error.response &&
+                    error.response.data &&
+                    error.response.data.message) ||
+                error.message ||
+                error.toString();
+            return thunkAPI.rejectWithValue(message);
+        }
+    }
+);
 
 export const authSlice = createSlice({
     name: "auth",
     intialState,
     reducers: {
         reset: (state) => {
-            state.isLoading = false
+            state.isLoading = false;
             state.isSuccess = false;
             state.isError = false;
-            state.message = '';
-        }
+            state.message = "";
+        },
     },
-    extraReducers: () => {}
+    extraReducers: () => {},
 });
 export const { reset } = authSlice.actions;
 export default authSlice.reducer;

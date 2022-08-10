7107d77aed3b1155a2fdbb7f98b9dcdf01a2d2d4
doniaskima <doniaskima344@gmail.com>
2022-08-11 00:29:04 +0100

    authSlice and authService

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,17 +1,38 @@
-import axios from 'axios';
-const API_URL = '/api/users/'
+import axios from "axios";
 
-//register user
+const API_URL = "/api/users/";
+
+// Register user
 const register = async(userData) => {
     const response = await axios.post(API_URL, userData);
+
     if (response.data) {
-        localStorage.setItem('user', JSON.stringify(response.data))
+        localStorage.setItem("user", JSON.stringify(response.data));
     }
-    return response.data
-}
+
+    return response.data;
+};
+
+// Login user
+const login = async(userData) => {
+    const response = await axios.post(API_URL + "login", userData);
+
+    if (response.data) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+    }
+
+    return response.data;
+};
+
+// Logout user
+const logout = () => {
+    localStorage.removeItem("user");
+};
 
 const authService = {
     register,
-}
+    logout,
+    login,
+};
 
 export default authService;

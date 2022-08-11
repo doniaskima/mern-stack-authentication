8b4a28e223831bc1c32cf92883ce17036a2cd18a
doniaskima <doniaskima344@gmail.com>
2022-08-11 14:47:33 +0100

    features

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,25 +1,48 @@
-import React from 'react'
 import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { logout, reset } from '../features/auth/authSlice'
 
 function Header() {
-  return (
-      <header className='header'>
-          <div className="logo">
-              <Link to='/'>Goalsetter</Link>
-          </div>
-          <ul>
-              <li>
-                  <Link to='/login'><FaSignInAlt/>Login</Link>
-              </li>
-              <li>
-                  <Link to='/register'><FaUser />register</Link>
-              </li>
-   
-          </ul>
-      </header>
-    
-  )
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const { user } = useSelector((state) => state.auth)
+
+    const onLogout = () => {
+        dispatch(logout())
+        dispatch(reset())
+        navigate('/')
+    }
+
+    return (
+        <header className='header'>
+            <div className='logo'>
+                <Link to='/'>GoalSetter</Link>
+            </div>
+            <ul>
+                {user ? (
+                    <li>
+                        <button className='btn' onClick={onLogout}>
+                            <FaSignOutAlt /> Logout
+                        </button>
+                    </li>
+                ) : (
+                    <>
+                        <li>
+                            <Link to='/login'>
+                                <FaSignInAlt /> Login
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/register'>
+                                <FaUser /> Register
+                            </Link>
+                        </li>
+                    </>
+                )}
+            </ul>
+        </header>
+    )
 }
 
 export default Header

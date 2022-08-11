ab8fde4718e449e4673a247888774dd20d0d5fb2
doniaskima <doniaskima344@gmail.com>
2022-08-11 14:08:02 +0100

    strug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 import Dashboard from './pages/Dashboard';
 import Header from './components/Header';
 import Login from './pages/Login';
 import Register from './pages/Register';
+ 
+
 function App() {
   return (
     <>
@@ -16,6 +20,7 @@ function App() {
           </Routes>
         </div>
       </Router>
+      <ToastContainer/>
     </> 
   );
 }
